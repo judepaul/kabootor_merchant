7604172b44aefca184d7a14818cf41501bb778ec
judesahayaraj <judepaul.arokiaraj@gmail.com>
2017-08-17 21:48:51 +0530

    Updated render for poster image

--- app/controllers/video_controller.rb
+++ app/controllers/video_controller.rb
@@ -3,14 +3,19 @@ class VideoController < ApplicationController
   skip_before_action :verify_authenticity_token, :only =>[:poster_image]
   
   def index
-    @file_name = "/photos/#{params[:file_name]}.png"
+    p "!!!!!!!!!!!"
+    p params[:file_name]
+    @file_name = "/photos/#{params[:file_name]}"
   end
   
   def poster_image
     file_name = convert_image_data_uri_to_image(params[:img_data], params[:archival_id]) if !params[:img_data].blank?
-    respond_to do |format|
-      format.html { redirect_to video_index_path(:file_name => file_name) } 
-    end
+    p "!!!!@@@@@@@ file_name => #{file_name}"
+    # file_name = "photo_9b44cfa0-8578-40d7-8665-40ec3011e3d0.png"
+    # respond_to do |format|
+    #   format.html { redirect_to video_index_path(:file_name => file_name, format: :html) }
+    # end
+    redirect_to video_index_path(:file_name => file_name, format: :html)
   end
   
   
