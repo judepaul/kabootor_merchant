4c5d1c35cfa55bea65c41d58f91311a618de7e20
judesahayaraj <judepaul.arokiaraj@gmail.com>
2017-08-17 20:07:20 +0530

    Worked on rendering poster image

--- app/controllers/video_controller.rb
+++ app/controllers/video_controller.rb
@@ -3,15 +3,16 @@ class VideoController < ApplicationController
   skip_before_action :verify_authenticity_token, :only =>[:poster_image]
   
   def index
+    p "@@@@@@@ ==> #{params[:file_name]}"
+    @file_name = params[:file_name]
   end
   
   def poster_image
-    p "!!!!!!!!!!!"
     file_name = convert_image_data_uri_to_image(params[:img_data], params[:archival_id]) if !params[:img_data].blank?
-    p params[:archival_id]
-    p file_name
+    file_name = "photo_9b44cfa0-8578-40d7-8665-40ec3011e3d0"
     respond_to do |format|
-          format.html {redirect_to("/video/index")}
+      p "!!!!!!!"
+      format.html { redirect_to video_index_path(:file_name => file_name) } 
     end
   end
   
