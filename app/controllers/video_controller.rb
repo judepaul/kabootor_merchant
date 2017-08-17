947b24ad4f32786da1ee7e3d3fd934d755247c66
judesahayaraj <judepaul.arokiaraj@gmail.com>
2017-08-17 20:56:38 +0530

    Worked on displaying image in video UI

--- app/controllers/video_controller.rb
+++ app/controllers/video_controller.rb
@@ -3,15 +3,12 @@ class VideoController < ApplicationController
   skip_before_action :verify_authenticity_token, :only =>[:poster_image]
   
   def index
-    p "@@@@@@@ ==> #{params[:file_name]}"
-    @file_name = params[:file_name]
+    @file_name = "/photos/#{params[:file_name]}.png"
   end
   
   def poster_image
     file_name = convert_image_data_uri_to_image(params[:img_data], params[:archival_id]) if !params[:img_data].blank?
-    file_name = "photo_9b44cfa0-8578-40d7-8665-40ec3011e3d0"
     respond_to do |format|
-      p "!!!!!!!"
       format.html { redirect_to video_index_path(:file_name => file_name) } 
     end
   end
