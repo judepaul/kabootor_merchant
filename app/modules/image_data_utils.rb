879d0532077ecce7d73a2d0e044e7aa8f6d3d910
judesahayaraj <judepaul.arokiaraj@gmail.com>
2017-08-18 19:38:16 +0530

    Worked on poster image display in video page

--- app/modules/image_data_utils.rb
+++ app/modules/image_data_utils.rb
@@ -3,11 +3,9 @@ module Image_data_utils
   require 'fileutils'
   
   def convert_image_data_uri_to_image(img_data, archival_id)
-    p "inside convert_image_data_uri_to_image"
     directory_name = "public/photos"
     Dir.mkdir(directory_name) unless File.exists?(directory_name)
-    file_name = "photo_#{archival_id}.png"
-    p "file_name --> #{file_name}"
+    file_name = "#{archival_id}.png"
     image_data = Base64.decode64(img_data['data:image/png;base64,'.length .. -1])
     new_file=File.new("#{directory_name}/#{file_name}", 'wb')
     new_file.write(image_data)
