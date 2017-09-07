0f70061cb1d4cdd85a222162a47ba2768b83a830
judesahayaraj <judepaul.arokiaraj@gmail.com>
2017-09-07 15:34:41 +0530

    Worked on post video creation dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,11 @@
 class DashboardController < ApplicationController
   
   def index
+    if params[:visitor_id].blank?
+      
+    else
+      @visitor = Visitor.find(params[:visitor_id])
+    end
   end
 
 end
